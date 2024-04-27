2f2db35600adf2c933511ab8d57bc1244db7fd8c
Aurora Moreno <aurora.moreno.lopez13@gmail.com>
2024-04-27 15:37:09 +0200

    initial commit

---
+++ push_swap.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aumoreno <aumoreno@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 08:57:11 by aumoreno          #+#    #+#             */
+/*   Updated: 2024/04/27 14:01:09 by aumoreno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+
+void push_swap()
+{
+	// vamos a empezar pintando la lista de números que nos llegue por consola 
+	
+	// si solo se puede pasar un numero a la vez, habrá que guardarlo en una lista de integer 
+	// no sé porque dudo probablemente vaya a tener que pasarla a una lista de ints
+}
+
+int main(int argc, char *argv[])
+{
+	    
+	push_swap();
+	return (0);
+}
